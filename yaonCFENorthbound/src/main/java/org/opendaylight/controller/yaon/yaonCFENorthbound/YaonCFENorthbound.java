2d6a43436011369b4e465461250642e1417d2e6f
Manpreet25 <manpreet.singh@nectechnologies.in>
2014-11-21 17:09:35 +0530

    Update Get Information Dummy support
    adding code for dummy support

--- yaonCFENorthbound/src/main/java/org/opendaylight/controller/yaon/yaonCFENorthbound/YaonCFENorthbound.java
+++ yaonCFENorthbound/src/main/java/org/opendaylight/controller/yaon/yaonCFENorthbound/YaonCFENorthbound.java
@@ -516,6 +516,268 @@ public class YaonCFENorthbound {
 		
 		return Response.ok(output).build();
 	}
+    
+    
+    /*Method that receive client rest api call for MAC information */
+
+    @Path("/GetInfo/Dummy")
+    @POST
+    @StatusCodes({
+        @ResponseCode(code = 200, condition = "Destination reachable"),
+        @ResponseCode(code = 503, condition = "Internal error"),
+        @ResponseCode(code = 503, condition = "Destination unreachable") })
+
+    public Response getallDummy(InputStream incomingData) {
+
+    	String output=null;
+        
+		
+        try {
+         	
+    		
+    		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+    		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+    		Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes"); 
+    		Document doc = docBuilder.newDocument();		
+    		Element rootElement = doc.createElement("Info");
+    		doc.appendChild(rootElement);
+    		
+    		Element slice1 = doc.createElement("slice");
+			rootElement.appendChild(slice1);
+		
+			Element first1 = doc.createElement("sliceId");
+			first1.appendChild(doc.createTextNode("2526"));
+			slice1.appendChild(first1);		
+		
+			Element second1 = doc.createElement("description");
+			second1.appendChild(doc.createTextNode("slice1"));
+			slice1.appendChild(second1);
+			
+			Element port11= doc.createElement("port");
+			slice1.appendChild(port11);
+			
+			Element firstP11 = doc.createElement("portId");
+			firstP11.appendChild(doc.createTextNode("1"));
+			port11.appendChild(firstP11);
+			
+			Element secondP11 = doc.createElement("dpId");
+			secondP11.appendChild(doc.createTextNode("00:00:00:00:00:00:00:01"));
+			port11.appendChild(secondP11);
+			
+			Element thirdP11 = doc.createElement("portname");
+			thirdP11.appendChild(doc.createTextNode("s1-eth2"));
+			port11.appendChild(thirdP11);
+			
+			Element fourthP11 = doc.createElement("vlanId");
+			fourthP11.appendChild(doc.createTextNode("8"));
+			port11.appendChild(fourthP11);
+			
+			Element fivethP11 = doc.createElement("description");
+			fivethP11.appendChild(doc.createTextNode("port1"));
+			port11.appendChild(fivethP11);
+			
+			Element sixthP11 = doc.createElement("state");
+			sixthP11.appendChild(doc.createTextNode("true"));
+			port11.appendChild(sixthP11);
+
+			Element mac111= doc.createElement("mac");
+			port11.appendChild(mac111);			
+
+			Element firstM111 = doc.createElement("MACaddress");
+			firstM111.appendChild(doc.createTextNode("1223232323"));
+			mac111.appendChild(firstM111);
+
+			Element secondM111 = doc.createElement("state");
+			secondM111.appendChild(doc.createTextNode("true"));
+			mac111.appendChild(secondM111);
+
+			Element mac112= doc.createElement("mac");
+			port11.appendChild(mac112);			
+
+			Element firstM112 = doc.createElement("MACaddress");
+			firstM112.appendChild(doc.createTextNode("1223232324"));
+			mac112.appendChild(firstM112);
+
+			Element secondM112 = doc.createElement("state");
+			secondM112.appendChild(doc.createTextNode("true"));
+			mac112.appendChild(secondM112);
+			
+			Element port12= doc.createElement("port");
+			slice1.appendChild(port12);
+			
+			Element firstP12 = doc.createElement("portId");
+			firstP12.appendChild(doc.createTextNode("2"));
+			port12.appendChild(firstP12);
+			
+			Element secondP12 = doc.createElement("dpId");
+			secondP12.appendChild(doc.createTextNode("00:00:00:00:00:00:00:02"));
+			port12.appendChild(secondP12);
+			
+			Element thirdP12 = doc.createElement("portname");
+			thirdP12.appendChild(doc.createTextNode("s2-eth2"));
+			port12.appendChild(thirdP12);
+			
+			Element fourthP12 = doc.createElement("vlanId");
+			fourthP12.appendChild(doc.createTextNode("8"));
+			port12.appendChild(fourthP12);
+			
+			Element fivethP12 = doc.createElement("description");
+			fivethP12.appendChild(doc.createTextNode("port2"));
+			port11.appendChild(fivethP12);
+			
+			Element sixthP12 = doc.createElement("state");
+			sixthP12.appendChild(doc.createTextNode("true"));
+			port12.appendChild(sixthP12);
+
+			Element mac121= doc.createElement("mac");
+			port12.appendChild(mac121);			
+
+			Element firstM121 = doc.createElement("MACaddress");
+			firstM121.appendChild(doc.createTextNode("1223232344"));
+			mac121.appendChild(firstM121);
+
+			Element secondM121 = doc.createElement("state");
+			secondM121.appendChild(doc.createTextNode("true"));
+			mac121.appendChild(secondM121);
+
+			Element mac122= doc.createElement("mac");
+			port12.appendChild(mac122);			
+
+			Element firstM122 = doc.createElement("MACaddress");
+			firstM122.appendChild(doc.createTextNode("ab23232323"));
+			mac122.appendChild(firstM122);
+
+			Element secondM122 = doc.createElement("state");
+			secondM122.appendChild(doc.createTextNode("true"));
+			mac122.appendChild(secondM122);
+
+			Element slice2 = doc.createElement("slice");
+			rootElement.appendChild(slice2);
+			
+			Element first2 = doc.createElement("sliceId");
+			first2.appendChild(doc.createTextNode("2527"));
+			slice2.appendChild(first2);		
+
+			Element second2 = doc.createElement("description");
+			second2.appendChild(doc.createTextNode("slice2"));
+			slice2.appendChild(second2);
+			
+			Element port21= doc.createElement("port");
+			slice2.appendChild(port21);
+			
+			Element firstP21 = doc.createElement("portId");
+			firstP21.appendChild(doc.createTextNode("3"));
+			port21.appendChild(firstP21);
+			
+			Element secondP21 = doc.createElement("dpId");
+			secondP21.appendChild(doc.createTextNode("00:00:00:00:00:00:00:03"));
+			port21.appendChild(secondP21);
+			
+			Element thirdP21 = doc.createElement("portname");
+			thirdP21.appendChild(doc.createTextNode("s3-eth2"));
+			port21.appendChild(thirdP21);
+			
+			Element fourthP21 = doc.createElement("vlanId");
+			fourthP21.appendChild(doc.createTextNode("8"));
+			port21.appendChild(fourthP21);
+			
+			Element fivethP21 = doc.createElement("description");
+			fivethP21.appendChild(doc.createTextNode("port1"));
+			port21.appendChild(fivethP21);
+			
+			Element sixthP21 = doc.createElement("state");
+			sixthP21.appendChild(doc.createTextNode("true"));
+			port21.appendChild(sixthP21);
+		
+			Element mac211= doc.createElement("mac");
+			port21.appendChild(mac211);			
+
+			Element firstM211 = doc.createElement("MACaddress");
+			firstM211.appendChild(doc.createTextNode("1423232323"));
+			mac211.appendChild(firstM211);
+
+			Element secondM211 = doc.createElement("state");
+			secondM211.appendChild(doc.createTextNode("true"));
+			mac211.appendChild(secondM211);
+
+			Element mac212= doc.createElement("mac");
+			port21.appendChild(mac212);			
+
+			Element firstM212 = doc.createElement("MACaddress");
+			firstM212.appendChild(doc.createTextNode("1253232324"));
+			mac212.appendChild(firstM212);
+
+			Element secondM212 = doc.createElement("state");
+			secondM212.appendChild(doc.createTextNode("true"));
+			mac212.appendChild(secondM212);
+			
+			Element port22= doc.createElement("port");
+			slice2.appendChild(port22);
+			
+			Element firstP22 = doc.createElement("portId");
+			firstP22.appendChild(doc.createTextNode("4"));
+			port22.appendChild(firstP22);
+			
+			Element secondP22 = doc.createElement("dpId");
+			secondP22.appendChild(doc.createTextNode("00:00:00:00:00:00:00:04"));
+			port22.appendChild(secondP22);
+			
+			Element thirdP22 = doc.createElement("portname");
+			thirdP22.appendChild(doc.createTextNode("s4-eth2"));
+			port22.appendChild(thirdP22);
+			
+			Element fourthP22 = doc.createElement("vlanId");
+			fourthP22.appendChild(doc.createTextNode("8"));
+			port22.appendChild(fourthP22);
+			
+			Element fivethP22 = doc.createElement("description");
+			fivethP22.appendChild(doc.createTextNode("port2"));
+			port22.appendChild(fivethP22);
+			
+			Element sixthP22 = doc.createElement("state");
+			sixthP22.appendChild(doc.createTextNode("true"));
+			port22.appendChild(sixthP22);
+
+			Element mac221= doc.createElement("mac");
+			port22.appendChild(mac221);			
+
+			Element firstM221 = doc.createElement("MACaddress");
+			firstM221.appendChild(doc.createTextNode("1223532344"));
+			mac221.appendChild(firstM221);
+
+			Element secondM221 = doc.createElement("state");
+			secondM221.appendChild(doc.createTextNode("true"));
+			mac221.appendChild(secondM221);
+
+			Element mac222= doc.createElement("mac");
+			port22.appendChild(mac222);			
+
+			Element firstM222 = doc.createElement("MACaddress");
+			firstM222.appendChild(doc.createTextNode("as23232323"));
+			mac222.appendChild(firstM222);
+
+			Element secondM222 = doc.createElement("state");
+			secondM222.appendChild(doc.createTextNode("true"));
+			mac222.appendChild(secondM222);
+    	    
+     		StringWriter stw = new StringWriter(); 
+     		transformer.transform(new DOMSource(doc), new StreamResult(stw)); 
+    		output = stw.getBuffer().toString();
+    	 
+    	  } catch (ParserConfigurationException pce) {
+    		pce.printStackTrace();
+    	  } catch (TransformerException tfe) {
+    		tfe.printStackTrace();
+    	  }
+        
+        if(output==null){
+        	
+        	return Response.ok(new String("Failed To Generate XML !")).build();
+        }
+		
+		return Response.ok(output).build();
+	}
 
     /*Method that receive client rest api call for whole network information */
 
@@ -566,7 +828,7 @@ public class YaonCFENorthbound {
     					for(ArrayList<String> portDetails:allPorts){
     						
     						Element port = doc.createElement("port");
-    	    				rootElement.appendChild(port);
+    						slice.appendChild(port);
     	    			
     	    				Element firstP = doc.createElement("portId");
     	    				firstP.appendChild(doc.createTextNode(portDetails.get(0)));
@@ -597,7 +859,7 @@ public class YaonCFENorthbound {
     	    	    			for(ArrayList<String> macDetails:allMac){
     	    	    			
     	    	    				Element mac = doc.createElement("mac");
-    	    	    				rootElement.appendChild(mac);
+    	    	    				port.appendChild(mac);
     	    	    			
     	    	    				Element firstM = doc.createElement("MACaddress");
     	    	    				firstM.appendChild(doc.createTextNode(macDetails.get(0)));
